be6a694ebc5a9f79c9dacdb20fd9d5a62286e312
Martin <88809775+Mz2007zm@users.noreply.github.com>
2021-11-15 19:20:57 -0800

    Button Placement and Fullscreen
    Made 2 Buttons
    FullScreen

--- GUI.py
+++ GUI.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox, RIGHT
 top = tk.Tk()
-top.geometry("500x500")
+top.geometry("1920x1080")
 # Code to add widgets will go here...
 
-def helloCallBack():
+def button1():
    tk.messagebox.showinfo( "Hello Python", "Hello World")
 
-B = tk.Button(top, text ="Hello", command = helloCallBack)
+B = tk.Button(top, text ="Hello", command = button1)
 
-B.pack()
+B.pack(side=RIGHT, padx=25, pady=25)
+
+# # # # #
+
+def button2():
+   tk.messagebox.showinfo( "Hello Python2", "Hello World2")
+
+Bu = tk.Button(top, text ="Hello2", command = button2)
+
+Bu.pack(side=RIGHT, padx=15, pady=20)
 top.mainloop()
-
-#helloCallBack()
